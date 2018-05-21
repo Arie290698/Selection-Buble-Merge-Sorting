c48f58c0ebd264f093d7f04398e627f10b633c0f
Arie290698 <33876123+Arie290698@users.noreply.github.com>
2018-05-22 06:31:34 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -48,6 +48,21 @@ int main(){
 		for (i = 0; i < (n); i++) {
 		    cout << data[i] << " ";
 		}
+		
+		cout << "\n\n\tSorted Data : ";
+		for (i = 0; i < (n - 1); i++) {
+		    for (j = 0; j < (n - i - 1); j++) {
+			if (data[j] > data[j + 1]) {
+			    temp = data[j];
+			    data[j] = data[j + 1];
+			    data[j + 1] = temp;
+			}
+		    }
+		}
+		for (i = 0; i < n; i++) {
+		    cout << data[i] << " ";
+		}
+		cout << endl;
 	break;
 			
 	case '3':
