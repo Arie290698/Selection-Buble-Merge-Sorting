c4e18902799713513ef4e42e7f7c2bbc53642bf8
zihadubaddika1004 <38626368+zihadubaddika1004@users.noreply.github.com>
2018-05-22 01:38:30 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,3 +5,10 @@ using namespace std;
 int data[]={77,42,35,12,101,5};
 int i, j, temp;
 int n=sizeof(data)/sizeof(data[0]);
+
+int main(){
+	cout<<"\n\t-=[ Nama Kelompok ]=-"<<endl;
+	cout<<"\t  - Arie Cahyadi (1717051056)\n\t  - Zihad"<<endl;
+	cout<<"\n\tPilihan Menu :\n\t1 Select Sorting\n\t2 Buble Sorting\n\t3 Merge Sorting"<<endl;
+	cout<<"\n\tMasukkan Pilihan : ";
+}
